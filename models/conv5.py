9a25d2a3ecfe07c93e30a4e5b4adbcd6acc2603e
cashel <cashel@utexas.edu>
2025-04-07 10:48:18 -0500

    Add batch normalization network

--- models/conv5.py
+++ models/conv5.py
@@ -68,3 +68,42 @@ class Conv5_small(nn.Module):
             return x, x_out
         else:
             return x
+        
+class Conv5_small_BN(nn.Module):
+    def __init__(self, num_classes=10, channels=1, loss_type='fedavg'):
+        super(Conv5_small_BN, self).__init__()
+        self.conv1 = nn.Conv2d(channels, 16, 3, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(32)
+        
+        self.conv3 = nn.Conv2d(32, 32, 3, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(32)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        
+        self.conv4 = nn.Conv2d(32, 32, 3, padding=1, bias=False)
+        self.bn4 = nn.BatchNorm2d(32)
+        
+        self.conv5 = nn.Conv2d(32, 32, 3, padding=1, bias=False)
+        self.bn5 = nn.BatchNorm2d(32)
+        self.pool3 = nn.MaxPool2d(2, 2)
+        
+        self.fc1 = nn.Linear(32 * 4 * 4, 256)
+        self.fc2 = nn.Linear(256, num_classes)
+        self.loss_type = loss_type
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.bn1(self.conv1(x))))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.pool2(F.relu(self.bn3(self.conv3(x))))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = self.pool3(F.relu(self.bn5(self.conv5(x))))
+        x_out = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x_out))
+        x = self.fc2(x)
+        if self.loss_type == 'fedmax':
+            return x, x_out
+        else:
+            return x
--- models/get_model.py
+++ models/get_model.py
@@ -1,4 +1,4 @@
-from models.conv5 import Conv5, Conv5_small
+from models.conv5 import Conv5, Conv5_small, Conv5_small_BN
 
 
 def get_model(model_name, loss_type='fedavg'):
@@ -6,5 +6,7 @@ def get_model(model_name, loss_type='fedavg'):
         return Conv5(loss_type=loss_type)
     elif model_name == "conv5small":
         return Conv5_small(loss_type=loss_type)
+    elif model_name == "conv5smallBN":
+        return Conv5_small_BN(loss_type=loss_type)
     else:
         raise NotImplementedError(f'[!] ERROR: Model {model_name} not implemented yet')
